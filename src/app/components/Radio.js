2ad73bcae2613afc5deeac9773dbf2ad97c5e5ff
James Santos <jamessantos614@gmail.com>
2024-01-12 16:22:53 -0300

    feat: component/radio

---
+++ src/app/components/Radio.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Radio = ({ name, value, id, checked, onChange }) => {
+  return (
+    <input type="radio" id={id} name={name} checked={checked} value={value} onChange={onChange} />
+  );
+};
+
+Radio.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Radio;
