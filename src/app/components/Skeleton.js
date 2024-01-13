173734577def4e023e9b86cd17c65e087fc41144
James Santos <jamessantos614@gmail.com>
2024-01-12 23:43:54 -0300

    feat: loading/skeleton

---
+++ src/app/components/Skeleton.js
@@ -0,0 +1,22 @@
+const Skeleton = ({ item }) => {
+  return (
+    <>
+      {[...Array(item)].map((_, index) => (
+        <div key={index} className="animate-pulse flex space-x-4 p-8">
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-2 bg-slate-700 rounded"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+                <div className="h-2 bg-slate-700 rounded col-span-1"></div>
+              </div>
+              <div className="h-2 bg-slate-700 rounded"></div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Skeleton;
