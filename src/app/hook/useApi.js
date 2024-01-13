3886ee335e5f8db9bc304f94b2a422d0f38d9521
James Santos <jamessantos614@gmail.com>
2024-01-13 15:07:29 -0300

    feat: api/application requests

--- src/app/hook/useApi.js
+++ src/app/hook/useApi.js
@@ -28,5 +28,89 @@ export default function useQuestions() {
     }
   };
 
-  return { result, isLoading, erro, fetchData };
+  const sendForm = async (formData) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setResult(data);
+      } else {
+        setError({});
+        throw new Error("Erro na requisição");
+      }
+    } catch (error) {
+      setError({});
+      console.error("Erro:", error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const sendError = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://fdlmx-backgrounds.sfo3.digitaloceanspaces.com/front-test/survey-post-error.json"
+      );
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setResult(data);
+      } else {
+        setError({});
+        throw new Error("Erro na requisição");
+      }
+    } catch (error) {
+      setError({});
+      console.error("Erro:", error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const sendSuccess = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://fdlmx-backgrounds.sfo3.digitaloceanspaces.com/front-test/survey-post-success.json"
+      );
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setResult(data);
+      } else {
+        setError({});
+        throw new Error("Erro na requisição");
+      }
+    } catch (error) {
+      setError({});
+      console.error("Erro:", error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    result,
+    isLoading,
+    erro,
+    fetchData,
+    sendForm,
+    sendError,
+    sendSuccess,
+  };
 }
